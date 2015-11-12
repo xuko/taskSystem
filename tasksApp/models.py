00b75ad34da54c639c29095908dedd9c7b74ad96
jorge <jorge.xuko@gmail.com>
2015-11-12 14:09:46 +0100

    added Tasksapp to settings

--- tasksApp/models.py
+++ tasksApp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Check(models.Model):
     text = models.CharField(max_length=200)
-    value = models.CharField(max_length=200)
+    value = models.CharField(max_length=100)
